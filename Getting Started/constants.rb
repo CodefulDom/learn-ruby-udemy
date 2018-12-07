a3f99abd8973ec292fd7f6d572220e6bf835ce5c
codefuldom <dominiquehallan@gmail.com>
2018-12-06 20:25:34 -0600

    completed section

---
+++ Getting Started/constants.rb
@@ -0,0 +1,10 @@
+NAME = 'Dominique Hallan'
+
+p NAME
+
+PI = 3.14159
+
+p PI
+
+BALLERS = 'MJ'
+p BALLERS
---
+++ Getting Started/object_methods.rb
@@ -0,0 +1,16 @@
+p 'Hello, World'.length
+p 'Hello, World'.upcase
+
+p 'HELLO WORLD'.downcase
+
+foo = 'Hello, Dom'
+p foo
+p foo.length
+p foo.upcase
+
+puts
+
+p 10.next
+p -1.next
+
+p 'This is a string'.inspect
