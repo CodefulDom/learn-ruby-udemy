2c6905b698500d76a315bb178fb1ceae3d59858f
codefuldom <dominiquehallan@gmail.com>
2018-12-06 20:55:19 -0600

    finished section

---
+++ Getting Started/string_interpolation.rb
@@ -0,0 +1,13 @@
+# String Interpolation is the process where we inject something into a string
+p 5
+
+p 5.next
+p 5.to_s
+
+name = 'Dom'
+
+p "Hello #{name}, how are you?"
+
+age = 35
+p "I am #{name}, and I am #{age}"
+p "The result of adding 1 + 1 is #{1 + 1}"
