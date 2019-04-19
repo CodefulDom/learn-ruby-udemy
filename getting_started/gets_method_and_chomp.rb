efa46492286e58ee16b1bff2fbbeb49323a37c30
codefuldom <dominiquehallan@gmail.com>
2019-04-19 08:16:11 -0500

    :tada: reformat project

---
+++ getting_started/gets_method_and_chomp.rb
@@ -0,0 +1,9 @@
+p 'Hey, what\'s you name?'
+name = gets.chomp
+
+p "Great! What's your age?"
+age = gets.chomp.to_i
+
+p "#{name}, you are #{age} years old and in 25 years you will be #{age + 25 } years old"
+
+p 10 + 3 + 4 + 9 + 6 + 13 + 6 + 4 + 8 + 9 + 10 + 15
