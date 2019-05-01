3528cbcc92f9718a91cc710ee0c3448b9e91a465
codefuldom <dominiquehallan@gmail.com>
2019-04-30 22:44:44 -0500

    :tada: WIP module

---
+++ strings/create_a_string.rb
@@ -0,0 +1,14 @@
+name = 'Dominqiue'
+p name
+
+puts 'Hi there, I am a string'
+
+revenue = '$23.00'
+puts revenue
+
+empty = ''
+p "The length of the variable empty is #{empty.length}"
+
+puts
+
+p empty.class
