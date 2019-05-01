35c3d9b397bc9c429de3be394387a6fbb2bfe66d
codefuldom <dominiquehallan@gmail.com>
2019-04-30 22:51:38 -0500

    :tada: completed module

---
+++ strings/create_a_string.rb
@@ -0,0 +1,19 @@
+name = 'Dominqiue'
+p name
+
+puts 'Hi there, I am a string'
+
+revenue = '$23.00'
+puts revenue
+
+empty = ''
+p "The length of the variable empty is #{empty.length}"
+
+puts
+
+p empty.class
+
+puts
+
+last_name = String.new('Hallan')
+p last_name
